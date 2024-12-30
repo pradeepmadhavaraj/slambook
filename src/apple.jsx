4d3f8f6901826205d66a407d9068bba6e12b987c
pradeepmadhavaraj <pradeepmadhavaraj@gmail.com>
2024-12-30 09:02:26 +0530

    first commit

---
+++ src/apple.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+class Apple extends Component{
+  render(){
+    return(<>
+    <h1>Nee yaru fill pani solu</h1>
+    </>
+    )
+  }
+    
+}
+export default Apple;
